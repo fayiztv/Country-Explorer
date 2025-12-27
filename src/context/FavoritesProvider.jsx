5ddd210a153a89f5b3eb122b5e5dfdcdbca57a39
Fayiz <tvfayiztv@gmail.com>
2025-12-27 13:41:06 +0530

    used context API to get the favoritess

---
+++ src/context/FavoritesProvider.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+export const FavoritesProvider = ({ children }) => {
+  const [favorites, setFavorites] = useState(() => {
+    const stored = localStorage.getItem("favorites");
+    return stored ? JSON.parse(stored) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }, [favorites]);
+
+  const addFavs = (country) => {
+    setFavorites((prev)=> [...prev, country])
+  }
+
+  const removeFavs = (code) => {
+    setFavorites((prev) => prev.filter((c)=> c.cca3 !== code))
+  }
+
+  const isFavs = (code) => {
+    return favorites.some((c) => c.cca3 === code)
+  }
+
+  return (
+    <FavoritesContext.Provider value={{favorites, addFavs, removeFavs, isFavs}} >
+        {children}
+    </FavoritesContext.Provider>
+  )
+};
---
+++ src/context/useFavorites.js
@@ -0,0 +1,6 @@
+import { useContext } from "react"
+import { FavoritesContext } from "./FavoritesContext"
+
+export const useFavorites = () => {
+    return useContext(FavoritesContext)
+}
